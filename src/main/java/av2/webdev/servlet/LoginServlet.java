739b419cb54e036b9fc5c2a8e710c26c7d7f76c7
Lucas Cavalheiro <lg.cavalheiro@hotmail.com>
2020-10-26 22:09:15 -0300

    fixed more things, got webservlet annotation running and testing travis-ci

--- src/main/java/av2/webdev/servlet/LoginServlet.java
+++ src/main/java/av2/webdev/servlet/LoginServlet.java
@@ -4,19 +4,17 @@ import av2.webdev.model.AccountDatabase;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet(name = "Login", urlPatterns = { "/login" })
 public class LoginServlet extends HttpServlet {
   private static final long serialVersionUID = -8259537970640399842L;
   AccountDatabase db = new AccountDatabase();
 
   @Override
-  protected void doGet(
-    HttpServletRequest request,
-    HttpServletResponse response
-  )
-    throws IOException {
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     try (PrintWriter out = response.getWriter()) {
       response.setContentType("text/html;charset=UTF-8");
       String result = db.getUserPassword(0);
@@ -26,8 +24,6 @@ public class LoginServlet extends HttpServlet {
   }
 
   @Override
-  protected void doPost(
-    HttpServletRequest request,
-    HttpServletResponse response
-  ) {}
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+  }
 }
