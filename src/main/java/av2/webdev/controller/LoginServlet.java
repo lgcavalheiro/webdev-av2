faf645fa6313f602532473940be130b8d899ffe3
Lucas Cavalheiro <lg.cavalheiro@hotmail.com>
2020-10-29 00:05:11 -0300

    made jstl, jsp and sass work

--- src/main/java/av2/webdev/controller/LoginServlet.java
+++ src/main/java/av2/webdev/controller/LoginServlet.java
@@ -3,13 +3,14 @@ package av2.webdev.controller;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import av2.webdev.database.AccountDatabase;
 
-@WebServlet(name = "Login", urlPatterns = { "/login" })
+@WebServlet(urlPatterns = { "/login" })
 public class LoginServlet extends HttpServlet {
   private static final long serialVersionUID = -8259537970640399842L;
   transient AccountDatabase db = new AccountDatabase();
@@ -19,12 +20,17 @@ public class LoginServlet extends HttpServlet {
     try (PrintWriter out = response.getWriter()) {
       response.setContentType("text/html;charset=UTF-8");
       String result = db.getUserPassword(123);
-      System.out.println(result);
-      out.println(result);
+      out.println("<head><meta charset=\"UTF-8\"></head>");
+      out.println("<body><h1>" + result + "</h1><body>");
     }
   }
 
   @Override
-  protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+    String id = request.getParameter("id");
+    String password = request.getParameter("password");
+    request.setAttribute("id", id);
+    request.setAttribute("password", password);
+    request.getRequestDispatcher("view/home.jsp").forward(request, response);
   }
 }
